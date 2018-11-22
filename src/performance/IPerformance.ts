205097f5b3e1b37f5cf6272d9f661e04df3e201e
Future New (WEILAI LIU) <New.Future@microsoft.com>
2018-11-22 23:48:57 +0800

    perf log

--- src/performance/IPerformance.ts
+++ src/performance/IPerformance.ts
@@ -2,7 +2,11 @@ export interface IPerformance {
     TableName: string;
     CorrelationId?: string;
     Extension?: any;
+
+    Transform: TransformFunction;
     log(action: string, duration: number, parameter?: any): void;
+    log(data: PerformanceParam): void;
+
 
     /**
      * 开始计时
@@ -11,17 +15,20 @@ export interface IPerformance {
      * @returns id
      */
     start(action: string, param?: any): number;
-    stop(id:number,extension?:any):void;
-    // clear(id:number);
+    start(data: PerformanceParam): number;
+    stop(id: number, extension?: any): boolean;
+    clear(id:number):boolean;
 }
 
-export interface PerformanceObject {
-    id: string;
-    record_time: number;
-    correlation_id?: string;
+export interface PerformanceParam {
+    [key: string]: any;
     action: string;
-    time: number;
-    user?: string
-    param?: string;
-    extension?: string;
+    time: number
+    correlation_id?: string;
+    user?: any
+    param?: any;
+    extension?: any;
+    record_time?: Date;
 }
+
+export type TransformFunction = (data: PerformanceParam) => { [key: string]: string | number | undefined };
